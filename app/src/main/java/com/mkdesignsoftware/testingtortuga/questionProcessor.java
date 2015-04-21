a6080f4041cec194b068469f8e52bd2257078a37
Matt Knouff <matt.knouff@mkdesignsoftware.com>
2015-04-21 19:46:11 -0400

    Started question processor. With a couple all-nighters, hopefully this will be ready for submission to the  Google Play store within the next week. All rights reserved to all code in this project, unless otherwise noted.

---
+++ app/src/main/java/com/mkdesignsoftware/testingtortuga/questionProcessor.java
@@ -0,0 +1,36 @@
+package com.mkdesignsoftware.testingtortuga;
+
+/**
+ * Question file processor and loading mechanism for Testing Tortuga.
+ * Shared class across all Testing Tortuga for _____ products.
+ * Created by mkdesign on 4/21/2015.
+ */
+
+public class questionProcessor {
+
+    String[] questionNumber;
+    String[] question;
+    String[] answerA;
+    String[] answerB;
+    String[] answerC;
+    String[] answerD;
+    String[] answerE;
+    String[] correctAnswer;
+    int numberOfQuestions;
+
+    public void loadQuestions throws FileNotException {
+        String TextData = textReader.readLine();
+        StringTokenizer st = new StringTokenizer(TextData, "|", false);
+
+        for (int i = 0; i < numberOfQuestions; i++) {
+            questionNumber[i] = st.nextToken();
+            question[i] = st.nextToken();
+            answerA[i] = st.nextToken();
+            answerB[i] = st.nextToken();
+            answerC[i] = st.nextToken();
+            answerD[i] = st.nextToken();
+            answerE[i] = st.nextToken();
+            correctAnswer[i] = st.nextToken();
+        }
+    }
+}
